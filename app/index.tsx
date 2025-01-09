5ce0816816a710a8287984d1c5686b2c58b9b75f
Chinmay <chinmay.singh@shipglobal.in>
2025-01-09 10:53:12 +0530

    added tailwind nativewind to project

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-thin text-xl">Index screen</Text>
     </View>
   );
 }

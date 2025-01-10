7f454c2b8cddceead2c1bbd40836b91c3f353423
Chinmay <chinmay.singh@shipglobal.in>
2025-01-10 10:43:21 +0530

    added gluestack and project desc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="/" options={{ headerShown: false }} />
-    </Stack>
+    <GluestackUIProvider mode="light"><Stack>
+        <Stack.Screen name="/" options={{ headerShown: false }} />
+      </Stack></GluestackUIProvider>
   );
 }

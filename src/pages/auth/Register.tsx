995ed3b869fc9c31462a6022a9e45fd7222ca158
Chinmay <chinmay.singh@shipglobal.in>
2025-01-13 12:50:21 +0530

    login ui completed

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export default function Register() {
   return (
     <Auth className="flex flex-col">
-      <p className="mt-2 text-black text-sm mb-4">Let's Get Started . . .</p>
-      <Button>Login with google</Button>
-      <Button className="mt-2">Login with email</Button>
+      <p className="mt-2 text-black text-sm">Let's Get Started . . .</p>
+      <Button className="mt-2">Register with google</Button>
+      <Button className="mt-2">Register with email</Button>
       <p className="mt-4 self-center text-sm">
         Already have an account?
         <Link
